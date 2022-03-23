261c15407de08b616c8b089579c60863d8a68226
DD-Hieu <doduchieu13101999@gmail.com>
2022-03-23 15:35:46 +0700

    Initial

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json());
+
+const courses = [
+    { id : 1, name : 'NodeJs'},
+    { id : 2, name : 'ReactJs'},
+    { id : 3, name : 'React Native'},
+]
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  });
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses)
+  });
+  
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(courses => courses.id === parseInt(req.params.id))
+    if(!course) res.status(404).send('ID not exist')
+    res.send(course);
+  });
+
+app.post('/api/courses/add', (req, res) => {
+    const course = {
+        id : req.body.id,
+        name : req.body.id,
+    }
+    courses.push(course)
+    res.send(JSON.stringify({
+        success : true,
+        notice : "Add success",
+        data : courses
+    }));
+  });
+
+app.put('/api/courses/edit/:id', (req, res) => {
+    const course = courses.find(courses => courses.id === parseInt(req.params.id))
+    course.name = req.body.name;
+    res.send(JSON.stringify({
+        success : true,
+        notice : "Edit success",
+        data : courses
+    }));
+  });
+
+app.delete('/api/courses/delete/:id', (req, res) => {
+    const course = courses.find(courses => courses.id === parseInt(req.params.id))
+    let index = courses.indexOf(course);
+    courses.splice(index, 1)
+    res.send(JSON.stringify({
+        success : true,
+        notice : "Delete success",
+        data : courses
+    }));
+  });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
