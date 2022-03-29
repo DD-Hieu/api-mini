7cffaf1c1a218ba80cab92304c57dbab40f48b9d
DD-Hieu <doduchieu13101999@gmail.com>
2022-03-29 10:05:31 +0700

    fix comment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.json());
 
-//Router
+//Basic-temp Router
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 //   });
